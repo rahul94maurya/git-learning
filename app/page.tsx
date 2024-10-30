f853f139906e71cf7a9ac13d19ba69aaf5b924bf
rahul demo <rahul@demo.com>
2024-10-30 17:07:12 +0530

    second feature commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <div>1st master commit </div>
       <div>2nd master commit </div>
       <div>1st feature commit</div>
+      <div>2nd feature commit </div>
     </div>
   );
 }
